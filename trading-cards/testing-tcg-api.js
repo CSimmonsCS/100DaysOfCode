d2c24093c5da147480583de1cee6adfd26fe43b3
Christian Simmons <CSimmonsCS@gmail.com>
2021-01-01 19:57:34 -0800

    Successful calls to API using bearer token & async to get all categories

--- trading-cards/testing-tcg-api.js
+++ trading-cards/testing-tcg-api.js
@@ -13,19 +13,63 @@ const dataString =
   '&client_id=' + PUBLIC_KEY +
   '&client_secret=' + PRIVATE_KEY;
 
-axios(
-  {
-    url: 'https://api.tcgplayer.com/token',
-    method: 'POST',
-    headers: headers,
-    data: dataString,
-  })
-.then((res) => {
-  console.log(res.data);
-  console.log('success');
-})
+var getBearerToken = async () => {
+  console.log('calling to api');
+  const response = await axios(
+    {
+      url: 'https://api.tcgplayer.com/token',
+      method: 'POST',
+      headers: headers,
+      data: dataString,
+    })
+  .catch(function (error){
+    console.log(error.response);
+    console.log('error');
+  });
 
-.catch(function (error){
-  console.log(error.response);
-  console.log('error');
-});
+  console.log('bearer token success');
+  const bearer = response.data.access_token;
+  headers['Authorization'] = 'bearer ' + bearer;
+  // console.log(headers);
+  // console.log(bearer);
+
+  // return bearer;
+}
+
+
+
+var getAllCategories = async () => {
+  console.log('calling to api');
+  const response = await axios({
+    url: 'https://api.tcgplayer.com/catalog/categories' + '?limit=16' + '&sortOrder=name',
+    method: 'GET',
+    headers: headers,
+  })
+  .catch((error) => {
+    console.log(error.response);
+  });
+
+
+  console.log('all categories call success');
+  //all categories
+  const all_categories = response.data;
+
+  return all_categories;
+
+}
+
+
+const bothCalls = async () => {
+  console.log('calling both functions');
+  await getBearerToken();
+  const categories = await getAllCategories();
+
+  return categories;
+}
+
+bothCalls().then( (res) =>
+  {
+    console.log(res);
+  }
+)
+// console.log(headers['bearer']);
