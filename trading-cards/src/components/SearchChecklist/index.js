397d6cd81c72a8951cce3113835270e518eff6c8
Christian Simmons <CSimmonsCS@gmail.com>
2021-01-13 23:24:30 -0800

    Factored out price_ranges in SearchChecklist using map function

--- trading-cards/src/components/SearchChecklist/index.js
+++ trading-cards/src/components/SearchChecklist/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const SearchChecklist = props => {
 
-  const printings = ['Holofoil', '1st Edition Holofoil', 'Reverse Holofoil', 'Normal'];
+  const printings = ['Holofoil',
+                     '1st Edition Holofoil',
+                     'Reverse Holofoil',
+                     'Normal'];
+
+  const prices = [5, 10, 25, 50, 100, 200, 500, 750];
 
   return (
     <div className="SearchChecklist">
@@ -24,62 +29,19 @@ const SearchChecklist = props => {
       </div>
       <h2>Price Limit</h2>
       <div className="search-price">
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 5)}
-            onChange={e => props.handleCheckboxPrinting(e, 5)}
-          />
-          <span>&#60;$5</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 10)}
-            onChange={e => props.handleCheckboxPrinting(e, 10)}
-          />
-          <span>&#60;$10</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 25)}
-            onChange={e => props.handleCheckboxPrinting(e, 25)}
-          />
-          <span>&#60;$25</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 50)}
-            onChange={e => props.handleCheckboxPrinting(e, 50)}
-          />
-          <span>&#60;$50</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 100)}
-            onChange={e => props.handleCheckboxPrinting(e, 100)}
-          />
-          <span>&#60;$100</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 200)}
-            onChange={e => props.handleCheckboxPrinting(e, 200)}
-          />
-          <span>&#60;$200</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 500)}
-            onChange={e => props.handleCheckboxPrinting(e, 500)}
-          />
-          <span>&#60;$500</span>
-        </label>
-        <label>
-          <input type="checkbox"
-            checked={props.checkedBoxes.find((ch) => ch === 750)}
-            onChange={e => props.handleCheckboxPrinting(e, 750)}
-          />
-          <span>&#60;$750</span>
-        </label>
+        {
+          prices.map(price => {
+            return (
+              <label>
+                <input type="checkbox"
+                  checked={props.checkedBoxes.find((ch) => ch === price)}
+                  onChange={e => props.handleCheckboxPrinting(e, price)}
+                />
+                <span>&#60;${price}</span>
+              </label>
+            );
+          })
+        }
       </div>
 
     </div>
